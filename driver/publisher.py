788631e7f17c1b794864afcfe474a3f5a637bfe9
nwags <nickwisc@gmail.com>
2016-01-18 11:40:49 -0500

    python object stuff, subscriber.py publisher.py

--- driver/publisher.py
+++ driver/publisher.py
@@ -3,7 +3,6 @@
 
 import json, collections
 
-
 out_dispatcher = {
 	'': lambda data: a(),
 	'': lambda data: b(),
@@ -13,47 +12,49 @@ out_dispatcher = {
 }
 
 
-def set_caller(session):
-	caller = session
-
-
-def dispatch_message(message):
-    try:
-    	dictum = collections.OrderedDict(json.loads(message.strip(), object_pairs_hook=collections.OrderedDict))
-    	if 'data' in dictum:
-    		dispatch(dictum['type'],dictum['data'])
-    	else:
-    		dispatch(dictum['type'],None)
-    except:
-    	print('*** error in publisher.dispatch_message ***')
-    	raise
-
-
-def dispatch(data):
-	if data is not None:
-		out_dispatcher[type_](self,data)
-	else:
-		out_dispatcher[type_](self)
-
-
-def send_message(self,type_,data_):
-	if caller is not None and type_ is not None:
-		if data_ is not None:
-			msg = {
-				'type':type_,
-				'data':damsg
-			}
-		else:
-			msg = {
-				'type':type_
-			}
-		try:
-			self.caller._myAppSession.publish('com.opentrons.driver_to_frontend',json.dumps(msg))
-		except:
-			print("error trying to send_message")
-
-
 class Publisher:
 	def __init__(self, session=None):
 		if session is not None:
 			caller = session
+
+
+	def set_caller(self, session):
+		caller = session
+
+
+	def dispatch_message(self, message):
+    	try:
+    		dictum = collections.OrderedDict(json.loads(message.strip(), object_pairs_hook=collections.OrderedDict))
+    		if 'data' in dictum:
+    			dispatch(dictum['type'],dictum['data'])
+    		else:
+    			dispatch(dictum['type'],None)
+    	except:
+    		print('*** error in publisher.dispatch_message ***')
+    		raise
+
+
+	def dispatch(self, data):
+		if data is not None:
+			out_dispatcher[type_](self,data)
+		else:
+			out_dispatcher[type_](self)
+
+
+	def send_message(self,type_,data_):
+		if caller is not None and type_ is not None:
+			if data_ is not None:
+				msg = {
+					'type':type_,
+					'data':damsg
+				}
+			else:
+				msg = {
+					'type':type_
+				}
+			try:
+				self.caller._myAppSession.publish('com.opentrons.driver_to_frontend',json.dumps(msg))
+			except:
+				print("error trying to send_message")
+
+
--- driver/subscriber.py
+++ driver/subscriber.py
@@ -13,34 +13,36 @@ in_dispatcher = {
 
 driver = None
 
-def set_driver(driver):
-    print("set_driver called")
-    driver = driver
-
-
-def dispatch_message(message):
-    print("dispatch_message called")
-    try:
-        dictum = collections.OrderedDict(json.loads(message.strip(), object_pairs_hook=collections.OrderedDict))
-        if 'data' in dictum:
-            in_dispatch(dictum['type'],dictum['data'])
-        else:
-            in_dispatch(dictum['type'],None)
-    except:
-        print('*** error in subscriber.dispatch_message ***')
-        raise
-
-
-def dispatch(data):
-    print("dispatch called")
-    if data is not None:
-        in_dispatcher[type_](self,data)
-    else:
-        in_dispatcher[type_](self)
-
-
 class Subscriber():
-    def __init__(self, driver=None):
+    def __init__(self, driver_=None):
+        driver = driver_
+
+    def set_driver(self, driver):
+        print("set_driver called")
         driver = driver
+
+
+    def dispatch_message(self, message):
+        print("dispatch_message called")
+        try:
+            dictum = collections.OrderedDict(json.loads(message.strip(), object_pairs_hook=collections.OrderedDict))
+            if 'data' in dictum:
+                in_dispatch(dictum['type'],dictum['data'])
+            else:
+                in_dispatch(dictum['type'],None)
+        except:
+            print('*** error in subscriber.dispatch_message ***')
+            raise
+
+
+    def dispatch(self, data):
+        print("dispatch called")
+        if data is not None:
+            in_dispatcher[type_](self,data)
+        else:
+            in_dispatcher[type_](self)
+
+
+
 
 
