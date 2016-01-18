5dc53209dcd89ebacca34fc5361fb53ee7defb37
nwags <nickwisc@gmail.com>
2016-01-18 10:34:05 -0500

    fix stupid indentation issue, print not FileIO, publisher.py

--- driver/publisher.py
+++ driver/publisher.py
@@ -18,39 +18,39 @@ def set_caller(session):
 
 
 def dispatch_message(message):
-	try:
-        dictum = collections.OrderedDict(json.loads(message.strip(), object_pairs_hook=collections.OrderedDict))
-        if 'data' in dictum:
-            dispatch(dictum['type'],dictum['data'])
-        else:
-            dispatch(dictum['type'],None)
+    try:
+    	dictum = collections.OrderedDict(json.loads(message.strip(), object_pairs_hook=collections.OrderedDict))
+    	if 'data' in dictum:
+    		dispatch(dictum['type'],dictum['data'])
+    	else:
+    		dispatch(dictum['type'],None)
     except:
-        FileIO.log('*** error in publisher.dispatch_message ***')
-        raise
+    	print('*** error in publisher.dispatch_message ***')
+    	raise
 
 
 def dispatch(data):
 	if data is not None:
-        out_dispatcher[type_](self,data)
-    else:
-        out_dispatcher[type_](self)
+		out_dispatcher[type_](self,data)
+	else:
+		out_dispatcher[type_](self)
 
 
 def send_message(self,type_,data_):
-    if caller is not None and type_ is not None:
-    	if data_ is not:
-	        msg = {
-	            'type':type_,
-	            'data':damsg
-	        }
+	if caller is not None and type_ is not None:
+		if data_ is not:
+			msg = {
+				'type':type_,
+				'data':damsg
+			}
 		else:
-	        msg = {
-	            'type':type_
-	        }
-	    try:
-	        self.caller._myAppSession.publish('com.opentrons.driver_to_frontend',json.dumps(msg))
-	    except:
-	        FileIO.log("error trying to send_message")
+			msg = {
+				'type':type_
+			}
+		try:
+			self.caller._myAppSession.publish('com.opentrons.driver_to_frontend',json.dumps(msg))
+		except:
+			print("error trying to send_message")
 
 
 class Publisher:
