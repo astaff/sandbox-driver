36ea39b9d6bdb062cab245a5ef0789e8bccb976c
nwags <nickwisc@gmail.com>
2016-01-28 15:07:55 -0500

    driver_harness.py dict reference name message

--- driver/driver_harness.py
+++ driver/driver_harness.py
@@ -159,8 +159,8 @@ class Harness(object):
 		print(data)
 		print()
 		if isinstance(data, dict):
-			name = data.name
-			value = data.message
+			name = data['name']
+			value = data['message']
 			if name in self.driver_dict:
 				if isinstance(value, dict):
 					message = list(value)[0]
@@ -179,8 +179,8 @@ class Harness(object):
 		print(data)
 		print()
 		if isinstance(data, dict):
-			name = data.name
-			value = data.message
+			name = data['name']
+			value = data['message']
 			print('name: ')
 			print(name)
 			print('value: ')
